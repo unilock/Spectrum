c5d7d40431383d53f75ff678b50a451f5ea40c52
azzyy <azzyypaaras@azzyys.works>
2024-11-18 16:39:42 -0600

    Immunity now immunities all over eternal slumber (it makes you immune to it. How immunizing.)

--- src/main/java/de/dafuqs/spectrum/registries/SpectrumStatusEffectTags.java
+++ src/main/java/de/dafuqs/spectrum/registries/SpectrumStatusEffectTags.java
@@ -8,13 +8,15 @@ import net.minecraft.registry.tag.*;
 
 public class SpectrumStatusEffectTags {
 	
-	public static TagKey<StatusEffect> INCURABLE;
+	public static TagKey<StatusEffect> NO_EFFECT_CLEAR;
+	public static TagKey<StatusEffect> IMMUNITY_IMMUNE;
 	public static TagKey<StatusEffect> NO_DURATION_EXTENSION;
 	public static TagKey<StatusEffect> SOPORIFIC;
 	public static TagKey<StatusEffect> NIGHT_ALCHEMY;
 
 	public static void register() {
-		INCURABLE = of("uncurable");
+		NO_EFFECT_CLEAR = of("unclearable");
+		IMMUNITY_IMMUNE = of("immunity_immune");
 		NO_DURATION_EXTENSION = of("no_duration_extension");
 		SOPORIFIC = of("soporific");
 		NIGHT_ALCHEMY = of("night_alchemy");
@@ -28,8 +30,12 @@ public class SpectrumStatusEffectTags {
 		return Registries.STATUS_EFFECT.getEntry(effect).isIn(tag);
 	}
 	
-	public static boolean isIncurable(StatusEffect statusEffect) {
-		return isIn(SpectrumStatusEffectTags.INCURABLE, statusEffect);
+	public static boolean isImmunityImmune(StatusEffect statusEffect) {
+		return isIn(SpectrumStatusEffectTags.IMMUNITY_IMMUNE, statusEffect);
+	}
+	
+	public static boolean isUnclearable(StatusEffect statusEffect) {
+		return isIn(SpectrumStatusEffectTags.NO_EFFECT_CLEAR, statusEffect);
 	}
 	
 	public static boolean hasEffectWithTag(LivingEntity livingEntity, TagKey<StatusEffect> tag) {
