a448b245321e1fe6904982e7481502f31a25402e
DaFuqs <dominik.m.schmidt@googlemail.com>
2024-11-29 15:00:36 +0100

    reduce azure aura particles (fps improvements)

--- src/main/java/de/dafuqs/spectrum/sound/BlockAuraSoundInstance.java
+++ src/main/java/de/dafuqs/spectrum/sound/BlockAuraSoundInstance.java
@@ -68,9 +68,10 @@ public class BlockAuraSoundInstance extends AbstractSoundInstance implements Tic
 		
 		if (volume > 0.25) {
 			Vec3d pos = new Vec3d(this.x, this.y, this.z);
-			ParticleHelper.playTriangulatedParticle(world, SpectrumParticleTypes.AZURE_AURA, Support.getIntFromDecimalWithChance(volume, random), true, new Vec3d(24, 8, 24), -8, true, pos, new Vec3d(0, 0.04D + random.nextDouble() * 0.06, 0));
-			ParticleHelper.playTriangulatedParticle(world, SpectrumParticleTypes.AZURE_MOTE_SMALL, Support.getIntFromDecimalWithChance(volume, random), false, new Vec3d(16, 8, 16), -6, false, pos, Vec3d.ZERO);
-			ParticleHelper.playTriangulatedParticle(world, SpectrumParticleTypes.AZURE_MOTE, Support.getIntFromDecimalWithChance(volume, random), true, new Vec3d(16, 6, 16), -4, false, pos, Vec3d.ZERO);
+			float chance = volume / 2F;
+			ParticleHelper.playTriangulatedParticle(world, SpectrumParticleTypes.AZURE_AURA, Support.getIntFromDecimalWithChance(chance, random), true, new Vec3d(24, 8, 24), -8, true, pos, new Vec3d(0, 0.04D + random.nextDouble() * 0.06, 0));
+			ParticleHelper.playTriangulatedParticle(world, SpectrumParticleTypes.AZURE_MOTE_SMALL, Support.getIntFromDecimalWithChance(chance, random), false, new Vec3d(16, 8, 16), -6, false, pos, Vec3d.ZERO);
+			ParticleHelper.playTriangulatedParticle(world, SpectrumParticleTypes.AZURE_MOTE, Support.getIntFromDecimalWithChance(chance, random), true, new Vec3d(16, 6, 16), -4, false, pos, Vec3d.ZERO);
 		}
 	}
 	
