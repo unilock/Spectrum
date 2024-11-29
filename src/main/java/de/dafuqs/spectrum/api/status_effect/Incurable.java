72831b0c65b066904449af0c236a3c74e6c1b7e5
azzyy <azzyypaaras@azzyys.works>
2024-11-28 19:41:44 -0600

    Star candies and whispy circlet now reduce incurable effect duration

--- src/main/java/de/dafuqs/spectrum/api/status_effect/Incurable.java
+++ src/main/java/de/dafuqs/spectrum/api/status_effect/Incurable.java
@@ -1,8 +1,12 @@
 package de.dafuqs.spectrum.api.status_effect;
 
 
+import de.dafuqs.spectrum.mixin.accessors.*;
 import de.dafuqs.spectrum.registries.SpectrumStatusEffects;
+import net.minecraft.entity.*;
 import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.network.packet.s2c.play.*;
+import net.minecraft.server.world.*;
 
 public interface Incurable {
 
@@ -17,4 +21,13 @@ public interface Incurable {
 
         return ((Incurable) instance).spectrum$isIncurable();
     }
+	
+	static void cutDuration(LivingEntity instance, StatusEffectInstance effect) {
+		// new duration = duration - 1min OR duration * 0.4, whichever is the smaller reduction
+		int duration = effect.getDuration();
+		((StatusEffectInstanceAccessor) effect).setDuration(Math.max(duration - 1200, (int)(duration * 0.4)));
+		if (!instance.getWorld().isClient()) {
+			((ServerWorld) instance.getWorld()).getChunkManager().sendToNearbyPlayers(instance, new EntityStatusEffectS2CPacket(instance.getId(), effect));
+		}
+	}
 }
